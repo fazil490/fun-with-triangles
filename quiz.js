1f3c6957abb54bc01310e93afffd344d36235d36
Fazil <mdif4900@gmail.com>
2022-05-14 11:40:12 +0530

    quiz - logic done

---
+++ quiz.js
@@ -0,0 +1,20 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitAnsBtn = document.querySelector("#submit-ans-btn");
+const outputEl = document.querySelector("#output");
+
+const correctAnswers = ["3", "65°", "Right angled", "90°"];
+
+function calculateScore() {
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for (let value of formResults.values()) {
+        if (value === correctAnswers[index]) {
+            score = score + 1;
+        }
+        index = index + 1;
+    }
+    outputEl.innerText = "Your score is " + score;
+}
+
+submitAnsBtn.addEventListener("click", calculateScore)
